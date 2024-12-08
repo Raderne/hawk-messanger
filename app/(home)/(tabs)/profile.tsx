b32f267de59edc9986d3834eb65c22231946d116
Reda El Marzouki <relmarzouki@gmail.com>
2024-12-08 14:06:01 +0100

    setup expo router

---
+++ app/(home)/(tabs)/profile.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function ProfileTabScreen() {
+  return <Text>profile tab</Text>;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Redirect } from "expo-router";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>My App</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Redirect href={"/(home)/(tabs)"} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
