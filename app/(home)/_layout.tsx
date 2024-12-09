8b5a8a311ba05371c802c05fc8f2226deb7c2904
Reda El Marzouki <relmarzouki@gmail.com>
2024-12-09 16:46:37 +0100

    add: Stream Chat Provider

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,41 +1,12 @@
-import { Slot, Stack } from "expo-router";
-import { useEffect } from "react";
-import { StreamChat } from "stream-chat";
-import { Chat, OverlayProvider } from "stream-chat-expo";
-
-const client = StreamChat.getInstance("79p558fkh968");
+import ChatProvider from "@/providers/chatProvider";
+import { Stack } from "expo-router";
 
 export default function HomeLayout() {
-  useEffect(() => {
-    const connect = async () => {
-      await client.connectUser(
-        {
-          id: "jlahey",
-          name: "Reda Lahey",
-          image: "https://i.imgur.com/fR9Jz14.png",
-        },
-        client.devToken("jlahey")
-      );
-
-      /**
-       *  Channel created using a channel id
-       */
-      // const channel = client.channel("messaging", "the_park", {
-      //   name: "The Park",
-      // });
-      // await channel.watch();
-    };
-
-    connect();
-  }, []);
-
   return (
-    <OverlayProvider>
-      <Chat client={client}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </Chat>
-    </OverlayProvider>
+    <ChatProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ChatProvider>
   );
 }
